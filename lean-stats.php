7474945bdf9a8eed63510654021f5a5c8a67b436
Victor <78041023+victor31657513@users.noreply.github.com>
2026-01-16 11:04:34 +0100

    Fix i18n json generation order (#74)

--- lean-stats.php
+++ lean-stats.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Lean Stats
  * Description: Privacy-friendly, self-hosted analytics for WordPress.
- * Version: 0.24.6
+ * Version: 0.24.7
  * Author: BimBeau
  * Author: Lean Stats
  * Text Domain: lean-stats
