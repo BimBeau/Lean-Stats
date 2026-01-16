f14498e462914ec7e546a95de28c8213e81aaa88
Victor <78041023+victor31657513@users.noreply.github.com>
2026-01-16 09:50:24 +0100

    Harden WP-CLI install and i18n checks

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,7 @@
 defined('ABSPATH') || exit;
 
 return [
-    'version' => '0.24.4',
+    'version' => '0.24.5',
     'slug' => 'lean-stats',
     'rest_namespace' => 'lean-stats/v1',
     'rest_namespace_internal' => 'lean-stats/internal/v1',
