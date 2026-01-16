ac25e2e19043753f8888fb3f8c3e445e74579e22
Victor <78041023+victor31657513@users.noreply.github.com>
2026-01-16 09:39:08 +0100

    Ensure translations in release zips

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,7 @@
 defined('ABSPATH') || exit;
 
 return [
-    'version' => '0.24.3',
+    'version' => '0.24.4',
     'slug' => 'lean-stats',
     'rest_namespace' => 'lean-stats/v1',
     'rest_namespace_internal' => 'lean-stats/internal/v1',
