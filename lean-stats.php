5071572ee97f38789398c70b24b54594e4d82e2a
Victor <78041023+victor31657513@users.noreply.github.com>
2026-01-20 18:18:30 +0100

    Add admin JS translation files for locales (#86)

--- lean-stats.php
+++ lean-stats.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Lean Stats
  * Description: Privacy-friendly, self-hosted analytics for WordPress.
- * Version: 0.24.16
+ * Version: 0.24.17
  * Author: BimBeau
  * Author: Lean Stats
  * Text Domain: lean-stats
