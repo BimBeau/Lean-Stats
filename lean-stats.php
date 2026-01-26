f6fb8a6b619356f6ef33c519d81dd2c9d9e3f1de
Victor <78041023+victor31657513@users.noreply.github.com>
2026-01-26 10:26:49 +0100

    Remove Data Views fallback table (#115)

--- lean-stats.php
+++ lean-stats.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Lean Stats
  * Description: Privacy-friendly, self-hosted analytics for WordPress.
- * Version: 0.41.3
+ * Version: 0.42.0
  * Author: BimBeau
  * Author: Lean Stats
  * Text Domain: lean-stats
