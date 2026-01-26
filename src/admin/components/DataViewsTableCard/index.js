02cc0a7c51b0f03b05e1c974f79afee69c72ede4
Victor <78041023+victor31657513@users.noreply.github.com>
2026-01-26 09:26:54 +0100

    Migrate top pages listing to DataViews

---
+++ src/admin/components/DataViewsTableCard/index.js
@@ -0,0 +1,51 @@
+import { DataViews } from '@wordpress/dataviews/wp';
+import { __, sprintf } from '@wordpress/i18n';
+
+import DataState from '../DataState';
+import LsCard from '../LsCard';
+
+const DEFAULT_LAYOUTS = {
+    table: {
+        showMedia: false,
+    },
+};
+
+const DataViewsTableCard = ({
+    title,
+    data,
+    fields,
+    view,
+    onChangeView,
+    isLoading,
+    error,
+    emptyLabel,
+    paginationInfo,
+    getItemId,
+    perPageSizes = [5, 10, 20],
+}) => (
+    <LsCard title={title}>
+        <DataState
+            isLoading={isLoading}
+            error={error}
+            isEmpty={!isLoading && !error && data.length === 0}
+            emptyLabel={emptyLabel}
+            loadingLabel={sprintf(__('Loading: %s', 'lean-stats'), title)}
+        />
+        {!isLoading && !error && data.length > 0 && (
+            <DataViews
+                data={data}
+                fields={fields}
+                view={view}
+                onChangeView={onChangeView}
+                paginationInfo={paginationInfo}
+                getItemId={getItemId}
+                defaultLayouts={DEFAULT_LAYOUTS}
+                search={false}
+                isLoading={isLoading}
+                config={{ perPageSizes }}
+            />
+        )}
+    </LsCard>
+);
+
+export default DataViewsTableCard;
