886d69405a3fd206a981351aa9963379ffd241c8
Victor <78041023+victor31657513@users.noreply.github.com>
2026-01-26 19:26:43 +0100

    Move 404 report into Pages tab (#122)

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2e0645f93bb952e2220d');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '626244d091bcd41afc30');
