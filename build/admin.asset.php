cda417de6758586e1aef1ca66538376ec4d37005
Victor <78041023+victor31657513@users.noreply.github.com>
2026-02-02 14:30:58 +0100

    Update daily page views chart styling

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4d6866da7e6998a66e87');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7ef75239148e832432ff');
