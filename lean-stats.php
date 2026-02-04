401716fb5ed47a36bbefba607338eccda5739655
Victor <78041023+victor31657513@users.noreply.github.com>
2026-02-04 15:49:00 +0100

    Update KPI badge styling

--- lean-stats.php
+++ lean-stats.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Lean Stats
  * Description: Privacy-friendly, self-hosted analytics for WordPress.
- * Version: 1.1.5
+ * Version: 1.1.6
  * Author: BimBeau
  * Author: Lean Stats
  * Text Domain: lean-stats
