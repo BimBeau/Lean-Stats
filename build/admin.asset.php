2693a74cf8dcdc427c74e37aea03f2e033008372
Victor <78041023+victor31657513@users.noreply.github.com>
2026-02-05 15:45:14 +0100

    Adjust top pages tab label and bump patch version (#167)

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8e3fa164800fa67e8c81');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '45351ee48887a668f6b4');
