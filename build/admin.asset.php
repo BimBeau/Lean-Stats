3255c5119e96ef15c4f8b54fe7252332186ae91e
Victor <78041023+victor31657513@users.noreply.github.com>
2026-02-05 14:13:09 +0100

    Affiche la date et les vues dans le tooltip du graphique

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '117db49655070495c9e5');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '478bc6c56339dd6facc2');
