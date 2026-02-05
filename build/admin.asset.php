f5febe99fb9b7db4fe147ff381d60ac0f769b2bd
Victor <78041023+victor31657513@users.noreply.github.com>
2026-02-05 14:29:08 +0100

    Update overview KPI loading skeleton cards (#155)

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bc360a99b9c0e2af2c47');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0e361aef152f10dfed40');
