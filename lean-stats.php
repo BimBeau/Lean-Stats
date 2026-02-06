6807aefaac37a129d7de5bc278097279b8d13f21
Victor <78041023+victor31657513@users.noreply.github.com>
2026-02-06 10:15:28 +0100

    Update admin label translations

--- lean-stats.php
+++ lean-stats.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Lean Stats
  * Description: Privacy-friendly, self-hosted analytics for WordPress.
- * Version: 1.3.13
+ * Version: 1.3.14
  * Author: BimBeau (Lean Stats)
  * Text Domain: lean-stats
  * Domain Path: /languages
