8eacfe96b60295c716d6dafba462b223547a41d9
Victor <78041023+victor31657513@users.noreply.github.com>
2026-02-06 10:45:38 +0100

    Fix duplicate i18n entries (#172)

--- lean-stats.php
+++ lean-stats.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Lean Stats
  * Description: Privacy-friendly, self-hosted analytics for WordPress.
- * Version: 1.3.15
+ * Version: 1.3.16
  * Author: BimBeau (Lean Stats)
  * Text Domain: lean-stats
  * Domain Path: /languages
