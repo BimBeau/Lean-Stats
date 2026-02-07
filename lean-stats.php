3c5da1d2ece12278696eb7d18ea0043c084d572d
Victor <78041023+victor31657513@users.noreply.github.com>
2026-02-07 16:13:05 +0100

    Améliorer l’accessibilité des données géographiques

--- lean-stats.php
+++ lean-stats.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Lean Stats
  * Description: Privacy-friendly, self-hosted analytics for WordPress.
- * Version: 1.10.0
+ * Version: 1.10.1
  * Author: BimBeau (Lean Stats)
  * Text Domain: lean-stats
  * Domain Path: /languages
