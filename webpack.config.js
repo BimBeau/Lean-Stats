fc7d909bde3245704eb00ff72af0f0d1b60ea527
Victor <78041023+victor31657513@users.noreply.github.com>
2026-02-07 17:16:09 +0100

    Add geojson support to build

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,16 @@ const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 
 module.exports = {
   ...defaultConfig,
+  module: {
+    ...defaultConfig.module,
+    rules: [
+      ...defaultConfig.module.rules,
+      {
+        test: /\.geojson$/i,
+        type: 'json',
+      },
+    ],
+  },
   entry: {
     admin: path.resolve(__dirname, 'src/admin/index.js'),
     'style-admin': path.resolve(__dirname, 'src/admin/style.css'),
