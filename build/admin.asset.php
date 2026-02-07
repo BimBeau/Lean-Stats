d52b57286de5f53880e5c93090dc0cbd780577c3
Victor <78041023+victor31657513@users.noreply.github.com>
2026-02-07 12:24:34 +0100

    Remove current lookup tab from geolocation

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5737c952278a5f11cb97');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7af5208a91fa713ab8c2');
